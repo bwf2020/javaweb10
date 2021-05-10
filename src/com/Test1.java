74e51301d90c59ad30f8e191faa63c2a0e46a59f
bwf <bwf@bwf.com>
2021-05-10 11:24:53 +0800

    day2

--- src/com/Test1.java
+++ src/com/Test1.java
@@ -17,14 +17,61 @@ public class Test1 extends HttpServlet{
 		
 		
 	}
+	
+	
 
-	@Override
+	
+	public void init() throws ServletException {
+
+
+			System.out.println("对象被初始化");
+		
+	}
+	
+
+
+
+
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		
+		System.out.println("service请求被调用了");
+		
+	    super.service(req, resp);
+
+		
+	}
+
+
+
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 
 		System.out.println("test1的doget方法.......");
 		
 	}
+
+
+
+
+	public void destroy() {
+
+
+		System.out.println("对象被销毁.........");
+		
+		
+		try {
+			Thread.sleep(0);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+	
 	
 	
 
